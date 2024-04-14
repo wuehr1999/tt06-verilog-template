4799d7df715b8afa5252099aaba25e3bc9537903
Jonas Wühr <33281076+wuehr1999@users.noreply.github.com>
2024-04-14 21:58:36 +0200

    Update project.v

--- src/project.v
+++ src/project.v
@@ -4,7 +4,25 @@
  */
 
 `define default_netname none
+module seg7 #( parameter BASE = 51 ) (
+    input wire [7:0] counter,
+    output reg [6:0] segments
+);
 
+    always @(*) begin
+      if(counter < BASE) begin
+        segments = 7'b0010000;
+      end else if(counter < 2 * BASE) begin
+        segments = 7'b0100000;
+      end else if(counter < 3 * BASE) begin
+        segments = 7'b0000001;
+      end else if(counter < 4 * BASE) begin
+        segments = 7'b0000010;
+      end else begin
+        segments = 7'b0000100;
+      end
+    end
+endmodule
 module tt_um_wuehr1999_servotester (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
