1931ee8c2f197ad5d4ae160c36e5758736df92cf
Jonas Wühr <33281076+wuehr1999@users.noreply.github.com>
2024-04-14 22:46:35 +0200

    Update project.v

--- src/project.v
+++ src/project.v
@@ -24,20 +24,7 @@ module tt_um_wuehr1999_servotester #( parameter MAX_COUNT = 200000, parameter MA
   assign uio_oe = 8'b11111111;
   assign uio_out[7] = signal < ui_in | counter > (MAX_COUNT - MAX_SIG * DEC_BASE);
   assign uio_out[6 : 0] = 0;
-  assign uo_out[7] = 0;
-  always @* begin
-      if(ui_in < DEC_BASE) begin
-        uo_out = 7'b0010000;
-      end else if(ui_in < 2 * DEC_BASE) begin
-        uo_out = 7'b0100000;
-      end else if(ui_in < 3 * DEC_BASE) begin
-        uo_out = 7'b0000001;
-      end else if(ui_in < 4 * DEC_BASE) begin
-        uo_out = 7'b0000010;
-      end else begin
-        uo_out = 7'b0000100;
-      end
-    end
+    assign uo_out = ui_in;
 
   always @(posedge clk) begin
     if(reset) begin
